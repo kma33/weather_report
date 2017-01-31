cc7628f968b06241c699a3f03837e8e1086066fa
Sam Schneider <sds6065@gmail.com>
2017-01-30 22:52:07 -0500

    bump to 1.4

--- mix.exs
+++ mix.exs
@@ -3,40 +3,36 @@ defmodule WeatherReport.Mixfile do
 
   def project do
     [app: :weather_report,
-     version: "0.1.0",
-     build_path: "../../_build",
-     config_path: "../../config/config.exs",
-     deps_path: "../../deps",
-     lockfile: "../../mix.lock",
-     elixir: "~> 1.2",
-     description: description,
-     package: package,
+     version: "0.2.0",
+     elixir: "~> 1.4",
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
-  
+
   def application do
-    [applications: [:logger, :feeder, :sweet_xml, :httpoison],
+    [extra_applications: [:logger],
      mod: {WeatherReport, []}]
   end
-  
+
   defp deps do
-    [{:feeder, "~> 2.0.0", compile: false},
+    [{:feeder, "~> 2.0.0"},
      {:sweet_xml, "~> 0.6.1"},
      {:httpoison, "~> 0.8.3"},
      {:earmark, "~> 0.2.1", only: :dev},
      {:ex_doc, "~> 0.11.4", only: :dev}]
   end
-  
+
   defp description do
     """
     Get weather forecasts from the National Oceanic and Atmospheric Administration!
-    
+
     As the NOAA is a United States government agency, only forecasts in the US are supported.
     """
   end
-  
+
   defp package do
     [
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
